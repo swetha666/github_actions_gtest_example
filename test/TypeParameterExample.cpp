4c38179a3b2b29d635cd301ca79aeb74716a32a5
scw1cob <c.swetha@in.bosch.com>
2023-07-27 14:55:14 +0530

    116

---
+++ test/TypeParameterExample.cpp
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+using testing::Types;
+
+class ITempSensor
+{
+    public:
+    virtual int getoutsideTemp()=0;
+};
+class ModelATempSensor:public ITempSensor
+{
+    public:
+    int getoutsideTemp(){return 23};
+};
+class ModelBTempSensor:public ITempSensor
+{
+    public:
+    int getoutsideTemp(){return 23};
+};
+class AutoTempRegulator
+{
+    ITempSensor* tempSensorPtr;
+    public:
+    AutoTempRegulator(ITempSensor* address):tempSensorPtr{address}{}
+    int regulateTemp()
+    {
+        return this ->tempSensorPtr->getOutSideTemp();
+    }
+};
+
+template <typename T>
+
+ITempSensor* createObject();
+template <>
+ITempSensor* createObject<ModelATempSensor>() { return new ModelATempSensor(); }
+template <>
+ITempSensor* createObject<ModelBTempSenso>() { return new ModelATempSensor(); }
+
+class TempSensorFixture:public testing::Test
+{
+    protected:
+    TempSensorFixture():objUnderTest{ createObject<T>()}
+    //Arrange
+    ITempSensor* objUnderTest;
+}
+
+typedef Types<ModelATempSensor,ModelBTempSensor> Implementations;
+
+TYPED_TEST_SUITE(TempSensorFixture, Implementations);
+
+TYPED_TEST(TempSensorFixture,GetTempTest)
+{
+    ASSERT_EQ(objUnderTest->getOutSideTemp(),23);
+}
