5629387ac9eeba73fdc567aab015862904043c43
scw1cob <c.swetha@in.bosch.com>
2023-07-27 15:35:11 +0530

    122

--- test/TypeParameterExample.cpp
+++ test/TypeParameterExample.cpp
@@ -1,53 +1,48 @@
 #include "gtest/gtest.h"
 using testing::Types;
 
-class ITempSensor
-{
+class ITempSensor{
     public:
-    virtual int getoutsideTemp()=0;
+    virtual int getOutSideTemp()=0;
 };
-class ModelATempSensor:public ITempSensor
-{
+class ModelATempSensor:public ITempSensor{
     public:
-    int getoutsideTemp(){return 23;}
+    int getOutSideTemp(){ return  23;}
 };
-class ModelBTempSensor:public ITempSensor
-{
+class ModelBTempSensor:public ITempSensor{
     public:
-    int getoutsideTemp(){return 23;}
+    int getOutSideTemp(){ return  23;}
 };
-class AutoTempRegulator
-{
+class AutoTempRegulator{
     ITempSensor* tempSensorPtr;
     public:
-    AutoTempRegulator(ITempSensor* address):tempSensorPtr{address}{}
-    void regulateTemp()
-    {
-        this ->tempSensorPtr->getoutsideTemp();
-    }
+    AutoTempRegulator(ITempSensor* address):tempSensorPtr{address} {}
+    void  regulateTemp(){
+         this->tempSensorPtr->getOutSideTemp();
+    }  
 };
 
 template <typename T>
-
 ITempSensor* createObject();
 template <>
 ITempSensor* createObject<ModelATempSensor>() { return new ModelATempSensor(); }
 template <>
-ITempSensor* createObject<ModelBTempSensor>() { return new ModelATempSensor(); }
+ITempSensor* createObject<ModelBTempSensor>() { return new ModelBTempSensor(); }
 
-class TempSensorFixture:public testing::Test
-{
+template <typename T>
+class TempSensorFixture:public testing::Test{
     protected:
     TempSensorFixture():objUnderTest{ createObject<T>()}{}
-    //Arrange
+    //arrange
     ITempSensor* objUnderTest;
-}
+
+};
 
 typedef Types<ModelATempSensor,ModelBTempSensor> Implementations;
 
 TYPED_TEST_SUITE(TempSensorFixture, Implementations);
 
-TYPED_TEST(TempSensorFixture,GetTempTest)
-{
-    ASSERT_EQ(this->objUnderTest->getoutsideTemp(),23);
+TYPED_TEST(TempSensorFixture,GetTempTest){
+    ASSERT_EQ(this->objUnderTest->getOutSideTemp(),23);
 }
+
